03f36262f384d88810ab86eddd90fa0d53beae1c
Nate Heydt <nate@tinfoilsecurity.com>
2018-06-05 01:34:25 -0700

    Rework issue parsing and rendering, migrate to attachment format for nicer Mattermost messages
    (Mostly) Separates parsing and display logic out of routes to make everything easier to follow and read.  Removes string building in favor of using composable units.  Changes some application defaults and assumptions in favor of more secure fallbacks if values aren't provided in the environment.

--- routes/index.js
+++ routes/index.js
@@ -2,115 +2,38 @@ var express = require('express');
 var router = express.Router();
 var https = require('https');
 var http = require('http');
-var toMarkdown = require('to-markdown');
 var url = require('url');
 var HttpsProxyAgent = require('https-proxy-agent');
 var HttpProxyAgent = require('http-proxy-agent');
 
-// const stdTTL = 240;
-// const cacheCheckPeriod = 60;
-
-const stdTTL = 240;
-const cacheCheckPeriod = 60;
-
-const NodeCache = require('node-cache');
-const recentUpdates = new NodeCache({
-    stdTTL: stdTTL,
-    checkperiod: cacheCheckPeriod
-});
-
-function recentlyUpdated(issueID) {
-    if (recentUpdates.get(issueID)) {
-        recentUpdates.ttl(issueID, stdTTL);
-        return true;
-    }
-    recentUpdates.set(issueID, true);
-    return false;
-}
-
-function isStatusTransition(changelog) {
-    if (!changelog) { return false; }
-    if (!changelog.items) { return false; }
-    var items = changelog.items;
-
-    if (Array.isArray(items)) {
-        let states = items.filter(t => t.field === "status" || t.field == "resolution");
-        return states.length > 0;
-    }
-    return false;
-}
-
-function toTitleCase(str) {
-    return str.replace(/\w\S*/g, function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-}
-
-function summarizeString(str, length) {
-    var out = str;
-    if (str.length > length) {
-        out = str.slice(0,length) + "\\[..\\]";
-    }
-
-    return out;
-}
-
-function toTable(items) {
-    const toTableEntry = (fieldName, fieldValue) =>
-        "| " + toTitleCase(toMarkdown(fieldName)) + " | " + toMarkdown(fieldValue) + " |";
-
-    var tableData = items.map(item => {
-        var fieldName = item.field;
-        var fieldValue = item.toString;
-        if (fieldValue) {
-            fieldValue = summarizeString(fieldValue, 20);
-        }
-        if(!fieldValue){
-            fieldValue = "-Cleared-";
-        }
-        return toTableEntry(fieldName, fieldValue);
-    });
-
-    tableData.unshift("| Field | Updated Value |\r\n|:----- |:-------------|");
-
-    return tableData.join("\r\n");
-}
-
-function summarizeIssue(issue) {
-    let fields = issue.fields;
-    let issuetype = fields.issuetype;
-    let name = issuetype.name;
-
-    var desc = fields.description || "";
-    desc = summarizeString(desc, 140);
-    if (desc.length > 0) {
-        desc = "\r\n\r\n" + desc;
-    }
-
-    return `Type: __${name}__${desc}`;
-}
+const Parsers = require('../lib/parsers');
+const IssueDebounce = require('../lib/issueDebounce');
+const EventTypes = require('../lib/eventTypes');
 
 function postToServer(postContent, hookid, matterUrl) {
-    console.log("Informing mattermost channel: " + hookid);
+    console.log("Informing mattermost channel");
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "1";
 
-    var agent, httpsagent, httpagent = null;
+    var agent, httpsagent, httpagent;
     var https_proxy = process.env.HTTPS_PROXY || process.env.https_proxy;
     var http_proxy = process.env.HTTP_PROXY || process.env.http_proxy;
     if(https_proxy)
     {
         httpsagent = new HttpsProxyAgent(https_proxy);
-        console.log("Using HTTPS proxy - " + https_proxy);
+        let log_safe = new url.URL(https_proxy).host;
+        console.log("Using HTTPS proxy - " + log_safe);
     }
-    if(http_proxy)
-    {
+    if(http_proxy) {
         httpagent = new HttpProxyAgent(http_proxy);
-        console.log("Using HTTP proxy - " + http_proxy);
+        let log_safe = new url.URL(http_proxy).host;
+        console.log("Using HTTP proxy - " + log_safe);
     }
+    // Prefer https agent over http agent
+    agent = httpsagent || httpagent;
 
     var matterServer = process.env.MATTERMOST_SERVER || 'localhost';
     var matterServerPort = process.env.MATTERMOST_SERVER_PORT;
-    var matterProto = process.env.MATTERMOST_SERVER_PROTO || 'http';
+    var matterProto = process.env.MATTERMOST_SERVER_PROTO || 'https';
     var matterPath = (process.env.MATTERMOST_SERVER_PATH || '/hooks/') + hookid;
     var matterUsername = process.env.MATTERMOST_USERNAME || 'JIRA';
     var matterIconUrl = process.env.MATTERMOST_ICON_URL || 'https://design.atlassian.com/images/logo/favicon.png';
@@ -119,57 +42,30 @@ function postToServer(postContent, hookid, matterUrl) {
     {
         try
         {
-            var murl = url.parse(matterUrl);
-            matterServer = murl.hostname || matterServer;
+            let murl = url.parse(matterUrl);
+
+            matterServer     = murl.hostname || matterServer;
             matterServerPort = murl.port || matterServerPort;
-            matterProto = murl.protocol.replace(":","") || matterProto;
-            matterPath = murl.pathname || matterPath;
+            matterProto      = murl.protocol.replace(":","") || matterProto;
+            matterPath       = murl.pathname || matterPath;
         }
         catch(err){console.log(err)}
     }
-    //If the port is not initialized yet (neither from env, nor from query param)
+    // If the port is not initialized yet (neither from env, nor from query param)
     // use the defaults ports
     if(!matterServerPort)
     {
-        if (matterProto == 'https')
-        {
-            matterServerPort = '443';
-        }
-        else
-        {
-            matterServerPort = '80';
-        }
+        matterServerPort = (matterProto === "http") ? "80" : "443";
     }
-    console.log(matterServer + "-" + matterServerPort  + "-" + matterProto);
+    console.log(`${matterServer} - ${matterServerPort} - ${matterProto}`);
 
     var proto;
-    if(matterProto == 'https')
-    {
-        console.log("Using https protocol");
-        proto = https;
-        agent = httpsagent;
-    }
-    else
-    {
-        console.log("Using http protocol");
-        proto = http;
-        agent = httpagent;
-    }
+    proto = (matterProto === 'http') ? http : https;
 
-    console.log("POST DATA");
-    console.log(postData);
+    postContent.username = matterUsername;
+    postContent.icon_url = matterIconUrl;
 
-    var postData;
-    try {
-        postData = JSON.stringify({
-            "text": postContent,
-            "username": matterUsername,
-            "icon_url": matterIconUrl
-        });
-    } catch(e) {
-        console.log("Malformed postContent - " + e);
-        return;
-    }
+    const postData = JSON.stringify(postContent);
 
     var post_options = {
         host: matterServer,
@@ -185,18 +81,16 @@ function postToServer(postContent, hookid, matterUrl) {
 
     try
     {
-        // Set up the request
         var post_req = proto.request(post_options, function(res) {
             res.setEncoding('utf8');
             res.on('data', function(chunk) {
-                console.log('Response: ' + chunk);
+                console.log('Response received');
             });
             res.on('error', function(err) {
                 console.log('Error: ' + err);
             });
         });
 
-        // post the data
         post_req.write(postData);
         post_req.end();
     }
@@ -219,84 +113,42 @@ router.get('/hooks/:hookid', function(req, res, next) {
 });
 
 router.post('/hooks/:hookid', function(req, res, next) {
-    console.log("Received update from JIRA");
+    console.log("Received event from JIRA");
     const hookId = req.params.hookid;
-    const webevent = req.headers['x-webhook-type'] || req.body.webhookEvent;
+    const webevent = req.body.webhookEvent;
+    const matterUrl = req.query.matterurl;
 
     if (!req.body.issue) {
         console.log(`Event (type ${webevent}) has no issue. Probably a buggy comment notification from https://jira.atlassian.com/browse/JRASERVER-59980`);
         if (req.body.comment.self) {
             console.log("...comment URL is " + req.body.comment.self);
         }
+        res.render('index', {title: ''});
         return;
     }
 
-    const matterUrl = req.query.matterurl;
+    if (!EventTypes.isEventType(webevent)) {
+        console.log("Ignoring events which we don't understand");
+        res.render('index', {title: ''});
+        return;
+    }
 
-    const issueID      = req.body.issue.key;
-    const issueRestUrl = req.body.issue.self;
-    const issueUrl     = new url.URL(`/browse/${issueID}`, issueRestUrl).toString();
-    const summary      = req.body.issue.fields.summary;
-
+    const issueID = req.body.issue.key;
     const changeLog = req.body.changelog;
-    const comment   = req.body.comment;
 
-    const author = (comment || {}).author;
-    const user   = req.body.user || author || {};
-
-    const displayName = user.displayName;
-    const avatar      = user.avatarUrls["16x16"];
-
-    var postContent;
-
-    const constructHeader = (action) =>
-        `##### ${displayName} ${action} [${issueID}](${issueUrl}): ${summary}`;
-
-    if (webevent === "jira:issue_updated")
-    {
-        if (recentlyUpdated(issueID)) {
+    // Check cache on issue updates to make sure we don't get spammed
+    // if there's a lot of updates in a small amount of time
+    if (EventTypes.isIssueUpdated(webevent)) {
+        if (IssueDebounce.recentlyUpdated(issueID)) {
             // Always send updates on status transitions, but not others
-            if (!isStatusTransition(changeLog)) {
-                res.render('index', {
-                    title: ''
-                });
+            if (!IssueDebounce.isStatusTransition(changeLog)) {
+                res.render('index', {title: ''});
                 return;
             }
         }
-        postContent = constructHeader('updated');
-    }
-    else if(webevent === "jira:issue_created")
-    {
-        postContent = constructHeader('created');
-        postContent += "\r\n" + summarizeIssue(req.body.issue)
-    }
-    else if(webevent === "jira:issue_deleted")
-    {
-        postContent = constructHeader('deleted');
-    }
-    else if(webevent === "comment_created")
-    {
-        postContent = constructHeader('commented on');
-    }
-    else
-    {
-        console.log("Ignoring events which we don't understand");
-        return;
     }
 
-    if(changeLog)
-    {
-        let changedItems = req.body.changelog.items;
-        let tableData = toTable(changedItems);
-
-        postContent += `\r\n${tableData}`;
-    }
-
-    if(comment)
-    {
-        let commentData = summarizeString(comment.body, 300);
-        postContent += "\r\n##### Comment:\r\n" + toMarkdown(commentData);
-    }
+    let postContent = Parsers.parseBody(req.body);
 
     postToServer(postContent, hookId, matterUrl);
 
